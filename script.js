94556b98fa0b8e66ed3b99b3524964f3d989e2ba
JessicaHeinze <inbox.jessicaheinze@gmail.com>
2019-03-05 00:34:22 +0100

    V0.8: added brick colission and breaking

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ let leftPressed = false;
 let interval = setInterval(draw, 10);
 
 // Brick wall
-let brickRowCount = 3;
-let brickColumnCount = 5;
+let brickRowCount = 5;
+let brickColumnCount = 3;
 let brickWidth = 75;
 let brickHeight = 20;
 let brickPadding = 10;
@@ -29,31 +29,46 @@ let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
 let brickY = (c * (brickHeight + brickPadding)) + brickOffsetTop;
 
 var bricks = [];
-for (var c = 0; c < brickColumnCount; c++) {
-    bricks[c] = [];
-    for (var r = 0; r < brickRowCount; r++) {
-        bricks[c][r] = {
-            x: 0,
-            y: 0
-        };
-    }
+for(var c=0; c<brickColumnCount; c++) {
+  bricks[c] = [];
+  for(var r=0; r<brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
 }
 
 function drawBricks() {
-    for (var c = 0; c < brickColumnCount; c++) {
-        for (var r = 0; r < brickRowCount; r++) {
-            var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-            var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#ff8fa7";
-            ctx.fill();
-            ctx.closePath();
+    for(var c=0; c<brickColumnCount; c++) {
+      for(var r=0; r<brickRowCount; r++) {
+        if(bricks[c][r].status == 1) {
+          var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
+          var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
+          bricks[c][r].x = brickX;
+          bricks[c][r].y = brickY;
+          ctx.beginPath();
+          ctx.rect(brickX, brickY, brickWidth, brickHeight);
+          ctx.fillStyle = "#ff8fa7";
+          ctx.fill();
+          ctx.closePath();
         }
+      }
     }
-}
+  }
+  
+    
+
+    function collisionDetection() {
+        for(var c=0; c<brickColumnCount; c++) {
+          for(var r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+            if(b.status == 1) {
+              if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                dy = -dy;
+                b.status = 0;
+              }
+            }
+          }
+        }
+      }
 
 // Controls
 document.addEventListener("keydown", keyDownHandler, false);
@@ -98,6 +113,7 @@ function draw() {
     drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection();
 
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
